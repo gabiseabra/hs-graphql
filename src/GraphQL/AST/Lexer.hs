b2e63165f761a235053b68f6484e12579bb25ef5
Gabriela Seabra <gabriela.lima.se@gmail.com>
2021-08-02 12:47:55 -0300

    parser

---
+++ src/GraphQL/AST/Lexer.hs
@@ -0,0 +1,203 @@
+{-# LANGUAGE
+    OverloadedStrings
+  , TypeApplications
+  , RankNTypes
+#-}
+
+module GraphQL.AST.Lexer
+  ( Parser
+  , whiteSpace
+  , lineTerminator
+  , ignoredTokens
+  , sc
+  , lexeme
+  , symbol
+  , braces
+  , parens
+  , brackets
+  , vars
+  , name
+  , enumVal
+  , boolVal
+  , intVal
+  , doubleVal
+  , stringVal
+  )
+  where
+
+import Control.Applicative ((<|>), empty)
+import Control.Monad (void)
+import Data.Void (Void)
+import Text.Megaparsec (Parsec, label, try, choice, optional, between, many, manyTill, some, oneOf, satisfy, takeP, chunkToTokens, notFollowedBy, sepBy, unPos)
+import Text.Megaparsec.Char (string, char)
+import qualified Text.Megaparsec.Char.Lexer as L
+import Data.Char (chr, digitToInt)
+import Data.Foldable (foldl', foldr)
+import Data.Scientific (toRealFloat)
+import Data.Maybe (isJust, fromMaybe)
+import qualified Data.List as List
+import Data.Text (Text)
+import qualified Data.Text as Text
+import Data.Proxy (Proxy(..))
+
+type Parser = Parsec Text Text
+
+upper, lower, alpha, ws, num :: String
+upper = "_ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+lower = "_acbcdefghijklmnopqrstuvwxyz"
+alpha = upper <> lower
+ws    = "\x0020\x0009"
+num   = ['0'..'9']
+
+upperChar, lowerChar, alphaChar, alphaNumChar :: Parser Char
+upperChar    = satisfy (`elem` upper)
+lowerChar    = satisfy (`elem` lower)
+alphaChar    = satisfy (`elem` alpha)
+alphaNumChar = satisfy (`elem` (alpha <> num))
+
+-- https://spec.graphql.org/June2018/#sec-White-Space
+whiteSpace :: Parser ()
+whiteSpace
+  = label "WhiteSpace" $ void $ satisfy (`elem` ws)
+-- http://spec.graphql.org/June2018/#sec-Line-Terminators
+lineTerminator :: Parser ()
+lineTerminator
+  = label "LineTerminator" $ void
+  $ choice
+    [ char '\x000A'
+    , char '\x000D' <* optional (char '\x000A')
+    ]
+-- http://spec.graphql.org/June2018/#sec-Insignificant-Commas
+insignificantComma :: Parser ()
+insignificantComma
+  = label "InsignificantComma" $ void $ char ','
+-- http://spec.graphql.org/June2018/#sec-Unicode
+unicodeBOM :: Parser ()
+unicodeBOM
+  = label "UnicodeBOM" $ void $ char '\xFEFF'
+-- http://spec.graphql.org/June2018/#sec-Source-Text.Ignored-Tokens
+ignoredTokens :: Parser ()
+ignoredTokens
+  = choice
+    [ whiteSpace
+    , lineTerminator
+    , insignificantComma
+    , unicodeBOM
+    ]
+
+-- http://spec.graphql.org/June2018/#sec-Comments
+sc :: Parser ()
+sc = L.space ignoredTokens (L.skipLineComment "#") empty
+
+lexeme :: Parser a -> Parser a
+lexeme = L.lexeme sc
+
+symbol :: Text -> Parser Text
+symbol = L.symbol sc
+
+between_ :: Parser sep -> Parser a -> Parser a
+between_ a = between a a
+
+braces, parens, brackets :: Parser a -> Parser a
+braces = between (symbol "{") (symbol "}")
+parens = between (symbol "(") (symbol ")")
+brackets = between (symbol "[") (symbol "]")
+
+vars :: (forall a. Parser a -> Parser a) -> Parser k -> Parser v -> Parser [(k, v)]
+vars f k v = label "Variables" $ f (many ((,) <$> (k <* symbol ":") <*> v))
+
+-- https://spec.graphql.org/June2018/#sec-Names
+name :: Parser Text
+name = label "Name" $ lexeme $ do
+  a  <- alphaChar
+  as <- many alphaNumChar
+  pure $ Text.pack (a:as)
+
+-- http://spec.graphql.org/June2018/#EnumValue
+enumVal :: Parser Text
+enumVal = label "EnumVal" $ lexeme $ Text.pack <$> some upperChar
+
+-- http://spec.graphql.org/June2018/#BooleanValue
+boolVal :: Parser Bool
+boolVal = label "BoolVal" $ lexeme $ choice
+  [ True  <$ string "true"
+  , False <$ string "false"
+  ]
+
+signed :: Num a => Parser a -> Parser a
+signed = L.signed empty
+
+-- http://spec.graphql.org/June2018/#sec-Int-Value
+intVal :: Parser Int
+intVal = label "IntVal" $ lexeme $ (signed L.decimal <* notFollowedBy (symbol "."))
+
+-- http://spec.graphql.org/June2018/#sec-Float-Value
+doubleVal :: Parser Double
+doubleVal = label "DoubleVal" $ lexeme $ (toRealFloat <$> signed L.scientific) <|> signed L.float
+
+-- http://spec.graphql.org/June2018/#SourceCharacter
+sourceChar
+  = '\x0009'
+  : '\x000A'
+  : '\x000D'
+  : ['\x0020' .. '\xFFFF']
+
+except :: String -> String -> String
+except = foldr List.delete
+
+-- http://spec.graphql.org/June2018/#EscapedUnicode
+-- http://spec.graphql.org/June2018/#EscapedCharacter
+-- https://github.com/bens/caraus-graphql/blob/cbccb9ed0b32167dbb4de16eb5143dd62f9f3159/src/Language/GraphQL/AST/Lexer.hs#L204
+escapeSequence :: Parser Char
+escapeSequence = do
+    void $ char '\\'
+    escaped <- oneOf ['"', '\\', '/', 'b', 'f', 'n', 'r', 't', 'u']
+    case escaped of
+        'b' -> return '\b'
+        'f' -> return '\f'
+        'n' -> return '\n'
+        'r' -> return '\r'
+        't' -> return '\t'
+        'u' -> chr . foldl' step 0
+                   . chunkToTokens (Proxy @Text)
+                 <$> takeP Nothing 4
+        _ -> return escaped
+  where
+    step accumulator = (accumulator * 16 +) . digitToInt
+
+sc' :: Parser ()
+sc' = L.space lineTerminator empty empty
+
+symbol' :: Text -> Parser Text
+symbol' = L.symbol sc'
+
+lineString :: Parser Text
+lineString = lexeme $ (Text.pack <$> between_ (symbol' "\"") (many chars))
+  where
+    chars = choice
+      [ escapeSequence
+      , satisfy (`elem` (sourceChar `except` "\n\""))
+      ]
+
+blockString :: Parser Text
+blockString = lexeme $ do
+  void $ delim
+  pos   <- L.indentLevel
+  lines <- manyTill chars delim `sepBy` try lineTerminator
+  pure $ Text.pack $ foldTrim (unPos pos) lines
+  where
+    delim = symbol' "\"\"\""
+    chars = choice
+      [ escapeSequence
+      , satisfy (`elem` sourceChar)
+      ]
+    indentLevel :: String -> Int
+    indentLevel "" = maxBound
+    indentLevel a = length $ List.takeWhile (`elem` ws) a
+    minIndent :: Int -> [String] -> Int
+    minIndent = foldr (min . indentLevel)
+    foldTrim :: Int -> [String] -> String
+    foldTrim i0 as = foldMap ((<> "\n") . List.drop (minIndent i0 as)) as
+
+stringVal :: Parser Text
+stringVal = label "StringVal" $ choice [ blockString, lineString ] <* sc
---
+++ src/GraphQL/AST/Parser.hs
@@ -0,0 +1,162 @@
+{-# LANGUAGE
+    OverloadedStrings
+  , DuplicateRecordFields
+  , TupleSections
+  , FlexibleContexts
+#-}
+
+module GraphQL.AST.Parser where
+
+import GraphQL.Class (Typename)
+import GraphQL.AST.Lexer (Parser)
+import qualified GraphQL.AST.Lexer as L
+
+import Text.Megaparsec (label, try, choice, optional, many, some, eitherP, eof, customFailure)
+import Text.Megaparsec.Char (string, char)
+import Data.Maybe (isJust, fromMaybe)
+import Data.Either (partitionEithers)
+import Data.Text (Text)
+import qualified Data.Text as Text
+
+data OperationType
+  = Query
+  | Mutation
+  | Subscription
+  deriving (Eq, Show)
+
+operationType :: Parser OperationType
+operationType = label "OperationType" $ L.lexeme $ choice
+  [ Query        <$ string "query"
+  , Mutation     <$ string "mutation"
+  , Subscription <$ string "subscription"
+  ]
+
+type VariableDefinition = (Typename, Bool)
+
+variableDefinitions :: Parser [(Text, VariableDefinition)]
+variableDefinitions = label "VariableDefinitions"
+  $ L.lexeme $ fmap (fromMaybe mempty) (optional $ L.vars L.parens k v)
+  where
+    k = char '$' *> L.name
+    v = (,) <$> L.name <*> (isJust <$> optional (char '!'))
+
+data VariableAssignment
+  = NullVal
+  | Var Text
+  | StrVal Text
+  | IntVal Int
+  | NumVal Double
+  | BoolVal Bool
+  | EnumVal Text
+  | ListVal [VariableAssignment]
+  | ObjectVal [(Text, VariableAssignment)]
+  deriving (Eq, Show)
+
+val :: Parser VariableAssignment
+val = label "Value" $ choice
+  [ NullVal   <$  L.symbol "null"
+  , Var       <$> (char '$' *> L.name)
+  , StrVal    <$> L.stringVal
+  , IntVal    <$> try L.intVal
+  , NumVal    <$> L.doubleVal
+  , BoolVal   <$> L.boolVal
+  , EnumVal   <$> L.enumVal
+  , ListVal   <$> L.brackets (many val)
+  , ObjectVal <$> L.vars L.braces L.name val
+  ]
+
+variableAssignments :: Parser [(Text, VariableAssignment)]
+variableAssignments = label "VariableAssignment"
+  $ fmap (fromMaybe mempty) (optional $ L.vars L.parens L.name val)
+
+data Field
+  = Field
+    { typename :: Maybe Typename
+    , alias :: Maybe Text
+    , name :: Text
+    , variables :: [(Text, VariableAssignment)]
+    } deriving (Eq, Show)
+
+field :: Parser Field
+field = label "Field" $ L.lexeme $
+  Field Nothing
+    <$> optional (try (L.name <* L.symbol ":"))
+    <*> L.name
+    <*> variableAssignments
+
+setTypename :: Typename -> Field -> Field
+setTypename ty f = f { typename = Just ty }
+
+data Selection
+  = Node Field [Selection]
+  | FragmentSpread Text
+  | InlineFragment Typename [Selection]
+  deriving (Eq, Show)
+
+selectionSet :: Maybe Typename -> Parser [Selection]
+selectionSet ty = label "SelectionSet" $ L.lexeme $ L.braces (some $ selection ty)
+
+selectionSet_ :: Maybe Typename -> Parser [Selection]
+selectionSet_ ty = fmap (fromMaybe mempty) (try $ optional $ selectionSet ty)
+
+selection :: Maybe Typename -> Parser Selection
+selection ty = label "Selection" $ L.lexeme $ choice
+  [ try selection'inlineFragment
+  , FragmentSpread <$> (L.symbol "..." *> L.name)
+  , Node           <$> fmap (maybe id setTypename ty) field
+                   <*> selectionSet_ ty
+  ]
+
+selection'inlineFragment :: Parser Selection
+selection'inlineFragment = do
+  ty     <- L.symbol "..." *> L.symbol "on" *> L.name
+  fields <- selectionSet $ Just ty
+  pure $ InlineFragment ty fields
+
+data Fragment
+  = Fragment
+    { typename :: Typename
+    , selectionFields :: [Selection]
+    } deriving (Eq, Show)
+
+fragment :: Parser (Text, Fragment)
+fragment = label "Fragment" $ L.lexeme $ do
+  name   <- L.symbol "fragment" *> L.name
+  ty     <- L.symbol "on" *> L.name
+  fields <- selectionSet $ Just ty
+  pure (name, Fragment ty fields)
+
+data Document
+  = Document
+    { operation :: OperationType
+    , name :: Maybe Text
+    , variables :: [(Text, VariableDefinition)]
+    , selectionFields :: [Selection]
+    , fragments :: [(Text, Fragment)]
+    } deriving (Eq, Show)
+
+type Operation = (OperationType, Maybe Text, [(Text, VariableDefinition)], [Selection])
+
+type RootNode = Either Operation Fragment
+
+rootNode = eitherP op fragment
+  where
+    op = label "Operation" $ L.lexeme $
+      (,,,) <$> operationType
+            <*> try (optional L.name)
+            <*> try variableDefinitions
+            <*> selectionSet Nothing
+
+document :: Parser Document
+document = label "Document" $ do
+  L.sc
+  ((op, name, vars, fields), frags) <- firstM validateOps . partitionEithers =<< some rootNode
+  L.sc *> eof
+  pure $ Document op name vars fields frags
+  where
+    validateOps []            = customFailure "Expected at least one operation, found none"
+    validateOps ops@(_:(_:_)) = customFailure $ "Expected at most one operation, found " <> Text.pack (show $ length ops)
+    validateOps (op:_)        = pure op
+
+firstM :: Functor m => (a -> m a') -> (a, b) -> m (a', b)
+firstM f (a,b) = (,b) <$> f a
